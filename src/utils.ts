e52ae2301ab379e782e10fd8711dc7cbe6b0913b
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2022-02-14 16:43:54 +0300

    utils updated

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ export function precisionRound(number: number, precision: number) {
     var factor = Math.pow(10, precision);
     return Math.round(number * factor) / factor;
 }
+
 export async function waitCallback<T>(_this: T, action, ...args): Promise<T> {
     return (new Promise((resolve, reject) => {
         action.call(_this, ...args, (err, result) => {
