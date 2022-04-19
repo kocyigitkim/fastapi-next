15344063449f44e6bb46b9159c23df7e19995f94
Muhammet Koçyiğit <muhammetkocyigit0@gmail.com>
2022-04-19 20:41:37 +0300

    added pre-check for knex dialects

--- build/plugins/NextKnexPlugin.js
+++ build/plugins/NextKnexPlugin.js
@@ -6,12 +6,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NextKnexPlugin = void 0;
 const knex_1 = __importDefault(require("knex"));
 const NextPlugin_1 = require("./NextPlugin");
+function configureKnexDialect(config) {
+    var clientName = null;
+    if (typeof config.client === 'string') {
+        clientName = config.client;
+    }
+    else if (typeof config.dialect === 'string') {
+        clientName = config.dialect;
+    }
+    if (clientName) {
+        switch (clientName) {
+            case "mssql":
+            case "mysql":
+            case "mysql2":
+            case "oracle":
+            case "oracledb":
+            case "pgnative":
+            case "postgres":
+            case "redshift":
+            case "sqlite3":
+            case "cockroachdb":
+                {
+                    config.client = require('knex/lib/dialects/' + clientName);
+                    if (config.dialect)
+                        delete config.dialect;
+                }
+                break;
+            default:
+                throw new Error("Unsupported client: " + clientName);
+                break;
+        }
+    }
+    return config;
+}
 class NextKnexPlugin extends NextPlugin_1.NextPlugin {
     constructor(config, pluginName = "db") {
         super(pluginName, true);
         this.config = config;
         this.pluginName = pluginName;
-        this.knex = (0, knex_1.default)(config);
+        this.knex = (0, knex_1.default)(configureKnexDialect(config));
     }
     async init(app) {
         app.log.info("Knex plugin loaded");
